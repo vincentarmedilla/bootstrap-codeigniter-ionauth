778cd570e8c4058f3fe566bde02dac92480c613f
erwin handoko <bhasunjaya@gmail.com>
2013-02-06 14:53:32 +0700

    test backends

---
+++ application/controllers/backends/user.php
@@ -0,0 +1,14 @@
+<?php
+
+class User extends MY_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->library('ion_auth');
+    }
+
+    function index() {
+        $this->render();
+    }
+
+}
